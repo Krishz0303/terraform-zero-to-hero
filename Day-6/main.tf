4c09f1c048a4701d3d365b3d1160c1ea47f6e1f1
TechSwankk <118037233+Krishz0303@users.noreply.github.com>
2024-06-30 14:11:12 +0530

    Update main.tf

--- Day-6/main.tf
+++ Day-6/main.tf
@@ -20,5 +20,5 @@ variable "instance_type" {
 module "ec2_instance" {
   source = "./modules/ec2_instance"
   ami = var.ami
-  instance_type = lookup(var.instance_type, terraform.workspace, "t2.micro")
+  instance_type = lookup(var.instance_type, terraform.workspace, "t2.micro") // Uses the lookup function to get the instance type based on the current Terraform workspace (environment) from the instance type map defined. If the workspace is not found in the instance_type map, it defaults to t2.micro.
 }
